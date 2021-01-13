c6bfacd9e9ee8d0d3d787f7da53dde61f151c43a
Scott <scott.wang@silverhorsetech.com>
2021-01-13 09:52:17 +0800

    addded editor field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {EditorFieldComponent} from './editor-field/editor-field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorFieldComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/editor-field/editor-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-field',
+  templateUrl: './editor-field.component.html',
+  styleUrls: ['./editor-field.component.scss']
+})
+export class EditorFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
